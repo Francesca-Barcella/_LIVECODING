0a3e8b186401602630e2f8cafc1b9e2d069a700e
Francesca-Barcella <pollon.fra@gmail>
2022-10-07 12:16:32 +0200

    qork-in-progress

--- JS - live/2022-10-07/assets/js/main.js
+++ JS - live/2022-10-07/assets/js/main.js
@@ -1,1 +1,24 @@
-alert('ciao, funziona!')
+/* 
+LiveCoding#1: 
+Ciao Visitatore!
+Imposta il nome di colore rosso in vari modi:
+Solo con HTML+CSS
+Scrivendo su element.style
+Aggiungendo la classe con JS
+*/
+
+//Scrivendo su element.style
+//1° seleziono l'elemento della dom che mi serve
+//2° - usare .style per settare il colore dell'elemento selezionato
+
+const titleElement = document.querySelector('h1');
+
+/* titleElement.style.color = 'yellow'; */
+
+//3° - abbino il colore tramite classe di js ma devo commentare quella al punto altrimenti non me la legge
+
+
+/* titleElement.className = 'green'; */
+
+titleElement.classList.add('blue')
+
