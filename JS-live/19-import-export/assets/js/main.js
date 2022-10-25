28aae7aad28cee19cd8d09acf06082c3bdc36d8f
Francesca-Barcella <pollon.fra@gmail>
2022-10-25 20:13:35 +0200

    recap debug

--- JS-live/19-import-export/assets/js/main.js
+++ JS-live/19-import-export/assets/js/main.js
@@ -1,5 +1,28 @@
-import {products} from "./db.js"
-
-
-
+import { products } from "./db.js"
 console.log(products);
+
+const cardElementDom = document.querySelector('.row');
+
+products.forEach(product => {
+    console.log(product);
+
+    const { image, name, price } = product;
+    console.log(image, name, price);
+
+    const productCardMarkUp =
+        `
+    <div class="col">
+        <div class="card">
+            <img>
+            <h4 class="title">name</h4>
+            <p class="text">description</p>
+            <p class="price">price</p>
+        </div>
+        <div>Buy It!</div>
+    </div>
+    `
+
+    cardElementDom.insertAdjacentElement(productCardMarkUp);
+
+
+})
---
+++ recap-debug/main.js
@@ -0,0 +1,110 @@
+/* function checkAge() {
+    const myAge = 32;
+    const message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+
+
+}
+checkAge(); */
+
+
+/* function checkAge() {
+    const myAge = 32;
+    let message = '';
+    console.log(myAge, message);
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+
+
+}
+checkAge(); */
+
+
+// ESERCIZIO DI GRUPPO
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter( (car) => car.type.toLowerCase() == 'benzina');
+
+const dieselCars = cars.filter( (car) => car.type.toLowerCase() == 'diesel');
+
+const otherCars = cars.filter( (car) => {
+    return car.type.toLowerCase() !== 'benzina' && car.type.toLowerCase() !== 'diesel';
+});
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+
+
+
+
+
+
+
