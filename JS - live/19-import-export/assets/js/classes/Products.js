e9dacf1f38284f0bd97e3dbbc8b3e5185822bcf7
Francesca-Barcella <pollon.fra@gmail>
2022-10-25 11:44:13 +0200

    live - work-in-progress

---
+++ JS - live/19-import-export/assets/js/classes/Products.js
@@ -0,0 +1,9 @@
+export default class Product {
+    constructor(name, img, id, price, description){
+        this.name = name;
+        this.img = img;
+        this.id = id;
+        this.price = price;
+        this.description = description;
+    }
+}
---
+++ JS - live/19-import-export/assets/js/db.js
@@ -0,0 +1,7 @@
+import Product from "./classes/Products.js";
+
+const mouse = new Product ('Lenovo', '', 57, 27.35, 'lorem5',);
+console.log(mouse);
+
+const monitor = new Product ('Hp', '', 33, 159.99, 'schermo 24 pollici',);
+console.log(monitor);
