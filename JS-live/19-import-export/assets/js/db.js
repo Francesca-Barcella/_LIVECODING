973c96f50af519ed4f5999b8fa69579b9e696a38
Francesca-Barcella <pollon.fra@gmail>
2022-10-26 10:45:45 +0200

    1--work-in-progress

--- JS-live/19-import-export/assets/js/db.js
+++ JS-live/19-import-export/assets/js/db.js
@@ -1,9 +1,13 @@
 import Product from "./classes/Product.js";
 
-const mouse = new Product ('Lenovo', '', 57, 27.35, 'mouse',);
-const monitor = new Product ('Hp', '', 33, 159.99, 'schermo 24 pollici',);
-const laptop = new Product ('Asusu', '', 33, 159.99, 'slaptop asus',);
+const mouse = new Product ('Lenovo', 'https://picsum.photos/200/300', 57, 27.35, 'mouse',);
+const monitor = new Product ('Hp', 'https://picsum.photos/200/300', 33, 159.99, 'schermo 24 pollici',);
+const laptop = new Product ('Asusu', './assets/img/partner.png', 33, 359.99, 'laptop asus',);
 
 const products =[ mouse, monitor, laptop];
 
 export {products};
+
+console.log(typeof Product + ' (typeof di Product)');
+console.log(typeof products + ' (typeof di products)');
+
--- JS-live/19-import-export/assets/js/main.js
+++ JS-live/19-import-export/assets/js/main.js
@@ -6,23 +6,23 @@ const cardElementDom = document.querySelector('.row');
 products.forEach(product => {
     console.log(product);
 
-    const { image, name, price } = product;
-    console.log(image, name, price);
+    const { name, image, price, description } = product;
+    console.log(name, image, price, description);
 
     const productCardMarkUp =
-        `
-    <div class="col">
-        <div class="card">
-            <img>
-            <h4 class="title">name</h4>
-            <p class="text">description</p>
-            <p class="price">price</p>
-        </div>
-        <div>Buy It!</div>
-    </div>
     `
+        <div class="col">
+            <div class="card">
+                <img src="https://picsum.photos/200/300" alt="">
+                <h4 class="title">${name}</h4>
+                <p class="price">€ ${price}</p>
+                <p class="text">${description}</p>
+                <div>Buy It!</div>
+            </div>
+        </div>
+    `;
 
-    cardElementDom.insertAdjacentElement(productCardMarkUp);
+    cardElementDom.insertAdjacentHTML( 'beforeend', productCardMarkUp);
 
 
 })
